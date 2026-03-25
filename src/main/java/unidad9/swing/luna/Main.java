eeb59a6c2c8edb2486131d104d1965a3080deb0f
Julio Pedro López Bayón <juliolb@educastur.org>
2026-03-25 20:40:41 +0100

    Actualización

---
+++ src/main/java/unidad9/swing/luna/Main.java
@@ -0,0 +1,58 @@
+package unidad9.swing.luna;
+
+import java.awt.BorderLayout;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+public class Main extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	private List<Luna> lunas = new LinkedList<>();
+	private JLabel luna;
+	
+	
+	public Main() {
+		super("Fases de la Luna");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		cargarLunas();
+		Luna luna = lunas.getFirst();
+		this.luna = new JLabel(luna.getNombre(), luna.getImagen(), JLabel.CENTER);
+		add(this.luna, BorderLayout.CENTER);
+		pack();
+		setLocationRelativeTo(null);
+	}
+	
+	private void cargarLunas() {
+		Luna luna;
+		ImageIcon imgLuna;
+		imgLuna = new ImageIcon(Main.class.getResource("/luna/Luna Nueva.png"));
+		luna = new Luna("Luna Nueva", imgLuna);
+		lunas.add(luna);
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> new Main().setVisible(true));
+	}
+	
+	private static class Luna {
+		private String nombre;
+		private ImageIcon imagen;
+		public Luna(String nombre, ImageIcon imagen) {
+			this.nombre = nombre;
+			this.imagen = imagen;
+		}
+		public String getNombre() {
+			return nombre;
+		}
+		public ImageIcon getImagen() {
+			return imagen;
+		}
+	}
+
+}
--- src/main/java/unidad9/swing/multicontador/Main.java
+++ src/main/java/unidad9/swing/multicontador/Main.java
@@ -1,18 +1,22 @@
 package unidad9.swing.multicontador;
 
 import java.awt.GridLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
-public class Main {
+public class Main extends WindowAdapter {
 	
 	private JFrame frame;
 	
 	Main(String[] args) {
         frame = new JFrame("Multi-Contador");
+        frame.addWindowListener(this);
         frame.getContentPane().setLayout(new GridLayout(0, 1));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         ContadorGlobal cg;
         cg = new ContadorGlobal();
         ContadorParcial cp;
@@ -34,4 +38,12 @@ public class Main {
     public static void main(final String[] args) {
         SwingUtilities.invokeLater(() -> new Main(args).show());
     }
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		int respuesta = JOptionPane.showConfirmDialog(frame, "¿Está seguro de que quiere cerrar la aplicación?");
+		if (respuesta == JOptionPane.YES_OPTION)
+			System.exit(0);
+	}
+
 }
