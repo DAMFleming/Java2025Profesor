0bb5cbdb2d17233663aa19095bcfcd8b15bc16e1
Julio Pedro López Bayón <juliolb@educastur.org>
2026-04-06 18:51:14 +0200

    Actualización

--- src/main/java/unidad9/java2d/ejemplo/Lienzo.java
+++ src/main/java/unidad9/java2d/ejemplo/Lienzo.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -29,6 +30,9 @@ public class Lienzo extends JPanel {
 	@Override
 	protected void paintComponent(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(
+				RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
 		g2d.drawImage(imagen, 0, 0, this);
 		g2d.setColor(Color.BLACK);
 		g2d.rotate(Math.PI / 4, 250, 200);
