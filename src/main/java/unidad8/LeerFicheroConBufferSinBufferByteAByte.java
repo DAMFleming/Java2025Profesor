7598d07c90c0c6135c1ece13b3baff671da3125c
Julio Pedro López Bayón <jplbayon@gmail.com>
2026-05-07 09:05:25 +0200

    Actualización

--- src/main/java/unidad8/LeerFicheroConBufferSinBufferByteAByte.java
+++ src/main/java/unidad8/LeerFicheroConBufferSinBufferByteAByte.java
@@ -8,21 +8,35 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class LeerFicheroConBufferSinBufferByteAByte {
+	
+	static void mostrarProgreso(long t0, long n, long c) {
+		long t1 = System.currentTimeMillis() - t0;
+		long s = (long) ((n - c) * t1 / c / 1000d);
+		long h = s / 3600;
+		s = s % 3600;
+		long m = s / 60;
+		s = s % 60;
+		IO.print(String.format("Leidos %d bytes de %d (%.2f%%) - Tiempo restante: %02d:%02d:%02d\033[0G", c, n, c * 100d / n, h, m, s));
+	}
 
 	static void leer(InputStream in, long n, String mensaje) throws IOException {
-		long c = 0;
 		IO.println(mensaje);
-		long t = System.currentTimeMillis();
+		long c = 0, t0 = System.currentTimeMillis(), t1, h, m, s;
+		long crono = t0;
 		while (in.read() != -1) {
 			c++;
-			IO.print(String.format("%.2f%% - %d / %d\033[0G", c * 100d / n, c, n));
+			if (System.currentTimeMillis() - crono > 1000 ) {
+				mostrarProgreso(t0, n, c);
+				crono = 0;
+			}
 		}
 		IO.println();
-		IO.println(String.format("\033[1B\033[0G%.2f segundos", (System.currentTimeMillis() - t) / 1000d));
+		IO.println(String.format("%.2f segundos", (System.currentTimeMillis() - t0) / 1000d));
+		IO.println();
 	}
-	
+
 	public static void main(String[] args) {
-		Path p = Path.of(System.getProperty("user.home"), "Desktop", "Debian 12.11 (LAMP).7Z");
+		Path p = Path.of("/home", "julio", "vmware", "Windows 11 x64", "Windows 11 x64-s001.vmdk");
 		try {
 			long n = Files.size(p);
 			try (FileInputStream in = new FileInputStream(p.toFile())) {
@@ -30,7 +44,6 @@ public class LeerFicheroConBufferSinBufferByteAByte {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
 			try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(p.toFile()))) {
 				leer(in, n, "Leyendo con buffer");
 			} catch (IOException e) {
