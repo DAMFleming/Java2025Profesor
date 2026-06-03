e3069cf11ef07ea99abaa1130943fb185b445fb7
Julio Pedro López Bayón <jplbayon@gmail.com>
2026-06-03 18:23:42 +0200

    Actualización

--- src/main/java/unidad3/arrays/Ejercicio11.java
+++ src/main/java/unidad3/arrays/Ejercicio11.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class Ejercici11 {
+public class Ejercicio11 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		Scanner sc = new Scanner(System.in);
---
+++ src/main/java/unidad3/regex/Ejercicio02.java
@@ -0,0 +1,23 @@
+package unidad3.regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Ejercicio02 {
+
+	public static void main(String[] args) {
+		Pattern e = Pattern.compile("\\d{2}-(\\d{2}|ene|feb|mar|abr|may|jun|jul|ago|sep|oct|nov|dic)-\\d{4}");
+		String fecha = IO.readln("fecha: ");
+
+		while (!fecha.equals("fin")) {
+			if (e.matcher(fecha).matches()) {
+				System.out.println("La fecha es correcta");
+			} else {
+				System.err.println("El formato de la fecha es incorrecta");
+			}
+			fecha = IO.readln("fecha: ");
+		}
+	}
+
+}
